694e29c2f2f5c245e6fc0b647c95437656529888
idevz <zhoujing_k49@163.com>
2018-12-02 09:12:53 +0800

    kernel diy

--- runpath/kube1/cilium/cilium-1.2.0/contrib/scripts/find-llvm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -x /usr/local/clang/bin/clang ]; then
-	echo "/usr/local/clang"
-	exit 0
-fi
-
-find / -iname libLLVMBPFCodeGen.a | head -1 | xargs dirname | xargs dirname
--- runpath/kube1/cilium/cilium-1.2.0/examples/minikube/README.rst
+++
@@ -1,4 +0,0 @@
-Getting Started with Cilium on minikube
-=======================================
-
-https://cilium.readthedocs.io/en/latest/gettingstarted/minikube
