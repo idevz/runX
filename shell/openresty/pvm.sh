e749053e2a69ec5d767063fc17c5ef9d26e1f0a7
idevz <zhoujing_k49@163.com>
2018-12-19 18:19:13 +0800

    add a simple luajit prove for ngx test

--- shell/openresty/pvm.sh
+++ shell/openresty/pvm.sh
@@ -2,3 +2,13 @@
 
 # default set to using debug version openresty
 dbg_or
+
+luajit_prove() {
+	if [ ! -z ${VALGRIND} ]; then
+		export TEST_NGINX_USE_VALGRIND=1
+	fi
+	# export TEST_NGINX_BENCHMARK_WARMUP=500
+	# export TEST_NGINX_BENCHMARK='2000000 20'
+	prove "$@"
+}
+export PATH=/usr/local/openresty-1.15.6.1rc0-debug/nginx/sbin:$PATH
