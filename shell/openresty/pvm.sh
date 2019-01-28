6a836c0a76a01449ef5701aa02a501a8f697345b
idevz <zhoujing_k49@163.com>
2019-01-29 07:07:54 +0800

    update runX his

--- shell/openresty/pvm.sh
+++ shell/openresty/pvm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# set -x
 # default set to using debug version openresty
 dbg_or
 
@@ -12,6 +12,30 @@ luajit_prove() {
 	prove "$@"
 }
 export PATH=/usr/local/openresty-1.15.6.1rc0-debug/nginx/sbin:$PATH
+
+mst_debug() {
+	x gdbserver :111 $(which openresty) \
+		-p ${RUN_PATH}/ngx.d \
+		-c $RUN_PATH/ngx.d/conf/mst-debug.conf &
+}
+
+pid_debug() {
+	set -x
+	echo "start or."
+	x $(which openresty) \
+		-p ${RUN_PATH}/ngx.d \
+		-c $RUN_PATH/ngx.d/conf/pid-debug.conf
+	sleep 1
+	local or_pid=$(ps aux | grep 'nginx: worker process' | grep -v grep | awk '{print $2}')
+	echo "get pid: ${or_pid}"
+	x gdbserver --attach :222 ${or_pid} &
+	set +x
+}
+
+stop_debug() {
+	x pkill gdbserver
+	x pkill openresty
+}
 
 start_debug_or() {
 	sudo pkill openresty
