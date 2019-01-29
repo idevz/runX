295bc61288a4eccb5eef270c84a7125c24f77252
idevz <zhoujing_k49@163.com>
2019-01-29 09:42:23 +0800

    update port for gdbserver

--- shell/openresty/pvm.sh
+++ shell/openresty/pvm.sh
@@ -14,19 +14,21 @@ luajit_prove() {
 export PATH=/usr/local/openresty-1.15.6.1rc0-debug/nginx/sbin:$PATH
 
 mst_debug() {
-	x gdbserver :111 $(which openresty) \
+	local gdb_server_port=111
+	x gdbserver :${gdb_server_port} $(which openresty) \
 		-p ${RUN_PATH}/ngx.d \
 		-c $RUN_PATH/ngx.d/conf/mst-debug.conf &
 }
 
 pid_debug() {
+	local gdb_server_port=222
 	x $(which openresty) \
 		-p ${RUN_PATH}/ngx.d \
 		-c $RUN_PATH/ngx.d/conf/pid-debug.conf
 	sleep 1
 	local or_pid=$(ps aux | grep 'nginx: worker process' | grep -v grep | awk '{print $2}')
 	echo "get pid: ${or_pid}"
-	x gdbserver --attach :222 ${or_pid} &
+	x gdbserver --attach :${gdb_server_port} ${or_pid} &
 }
 
 stop_debug() {
