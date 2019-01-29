f30d35ccd8a42dc03f10e3efeca7e6da62bbb36e
idevz <zhoujing_k49@163.com>
2019-01-29 18:55:11 +0800

    try to get golang assembly

--- shell/golang/pvm.sh
+++ shell/golang/pvm.sh
@@ -4,3 +4,8 @@
 export_go_path
 
 alias dk_go_build='CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo'
+
+get_assembly() {
+	# https://colobu.com/2018/12/29/get-assembly-output-for-go-programs/
+	go tool compile -N -l -S once.go
+}
